7cc382edb34368ec2603aeedc8bfc82a6475237d
nic hart <nic@hdb3.net>
2019-04-22 13:07:44 +0100

    disable bgpd as a listner on port 179

--- frr/run.sh
+++ frr/run.sh
@@ -1,4 +1,4 @@
 /usr/lib/frr/bgpd --version |& cat > /var/www/thttpd/version.txt
 /usr/bin/Sysinfo /> var/www/thttpd/sysinfo.txt
 
-/usr/lib/frr/bgpd -n --log stdout -f /etc/frr/bgpd.conf
+/usr/lib/frr/bgpd -p 0 -n --log stdout -f /etc/frr/bgpd.conf
