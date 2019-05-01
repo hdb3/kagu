90e39ef1d7f5c10e5da5b55e755605ddf3fb25d2
nic hart <nic@hdb3.net>
2019-05-01 12:08:01 +0100

    (mostly) working version of hbgp

--- hbgp/run.sh
+++ hbgp/run.sh
@@ -1,10 +1,11 @@
 # run.sh for hbgp
+ip addr add 169.254.0.1/32 dev lo || echo "probabaly the address is already assigned"
 if [ "$1" = "sysinfo" ]
 then
   echo "VERSION='`/usr/bin/Router --version|head -1`'"
   /usr/bin/Sysinfo
 else
   echo "starting hbgp"
-  sleep 10
+  cd /root
   /usr/bin/hbgp
 fi
