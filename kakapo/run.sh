fccc9e8c1d38fe3efdf3e690fa6fe070452e2322
nic hart <nic@hdb3.net>
2019-05-07 21:43:15 +0100

    WiP

--- kakapo/run.sh
+++ kakapo/run.sh
@@ -1,3 +1,18 @@
-ip addr add 169.254.0.11/32 dev lo || echo "probabaly the address is already assigned"
-ip addr add 169.254.0.12/32 dev lo || echo "probabaly the address is already assigned"
-/usr/sbin/sshd -4 -D -p 65535
+
+
+
+
+# run.sh for kakapo
+if [ "$1" = "relay" ]
+then
+  echo "starting kakapo-relay"
+  ip addr add 169.254.0.13/32 dev lo || echo "probabaly the address is already assigned"
+  ip addr add 169.254.0.14/32 dev lo || echo "probabaly the address is already assigned"
+  /usr/bin/relay ./relay 169.254.0.11,169.254.0.13,64504 169.254.0.12,169.254.0.14,64504
+else
+  echo "starting kakapo"
+  ip addr add 169.254.0.11/32 dev lo || echo "probabaly the address is already assigned"
+  ip addr add 169.254.0.12/32 dev lo || echo "probabaly the address is already assigned"
+  /usr/sbin/sshd -4 -D -p 65535
+fi
+
