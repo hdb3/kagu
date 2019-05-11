4caddaed8fe020b2fb3781c417407839b8ec7df6
nic hart <nic@hdb3.net>
2019-05-11 08:07:48 +0100

    before localising

--- kakapo/run.sh
+++ kakapo/run.sh
@@ -3,7 +3,22 @@
 
 
 # run.sh for kakapo
-if [ "$1" = "relay" ]
+if [ "$1" = "cdump" ]
+then
+  echo "testing core dumps"
+  /root/cdump
+  if [ ls core* ]
+  then
+    echo "core dump(s) found"
+    for core in `ls core*`
+    do
+      curl -X PUT --data-binary @${core} http://10.30.65.209/coredump/${core}
+      echo "put core file $core"
+    done
+    curl -X PUT --data-binary @/root/cdump http://10.30.65.209/coredump/cdump
+      echo "put executable file too"
+  fi
+elif [ "$1" = "relay" ]
 then
   echo "starting kakapo-relay"
   ip addr add 172.18.0.13/32 dev lo || echo "probabaly the address is already assigned"
@@ -15,4 +30,3 @@ else
   ip addr add 172.18.0.12/32 dev lo || echo "probabaly the address is already assigned"
   /usr/sbin/sshd -4 -D -p 65535
 fi
-
