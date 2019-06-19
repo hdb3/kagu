d3e47d3f6f6a6bdfe5ea09273def571ba71b38b1
nic hart <nic@hdb3.net>
2019-06-19 10:58:17 +0100

    on stack journey

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 REPO="localrepo:5000"
-baseimages="ghc base"
+baseimages="stack ghc base"
 exportimages="kakapo relay hbgp bird frr"
 for i in $baseimages $exportimages ; do docker build -t $i $i ; done
 for ei in $exportimages ; do docker tag ${ei} ${REPO}/${ei}; docker push ${REPO}/${ei} ; done
