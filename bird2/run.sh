9db1718b196416cadf5fe2caeccd6a247e012737
nic hart <nic@hdb3.net>
2019-07-02 13:21:55 +0100

    add bird2

---
+++ bird2/run.sh
@@ -0,0 +1,11 @@
+# run.sh for bird
+if [ "$1" = "sysinfo" ]
+then
+  echo "VERSION='`/usr/sbin/bird --version |& cat`'"
+  /usr/bin/sysinfo
+else
+  echo "starting bird"
+  ip addr add 172.18.0.13/32 dev lo || echo "probably the address is already assigned"
+  until fping -S 172.18.0.13 172.18.0.21 172.18.0.22 ; do echo "retry" ; done
+  /usr/sbin/bird -d
+fi
