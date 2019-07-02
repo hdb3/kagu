19aabd1c399a60ab2710e2cd63150c140fc3f4a9
nic hart <nic@hdb3.net>
2019-07-02 14:31:05 +0100

    add (open)bgpd

---
+++ bgpd/run.sh
@@ -0,0 +1,10 @@
+if [ "$1" = "sysinfo" ]
+then
+  echo "VERSION='openbgpd-6.5p0'"
+  /usr/bin/sysinfo
+else
+  echo "starting bird"
+  ip addr add 172.18.0.13/32 dev lo || echo "probably the address is already assigned"
+  until fping -S 172.18.0.13 172.18.0.21 172.18.0.22 ; do echo "retry" ; sleep 5 ; done
+  /usr/sbin/bgpd -d
+fi
