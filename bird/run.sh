6e35bb369ebdf8c593d15e1c40f1418fb1fc2b95
Nicholas Hart <nic@hdb3.net>
2020-04-15 17:06:36 +0100

    simplifying for standalone operations

--- bird/run.sh
+++ bird/run.sh
@@ -5,7 +5,8 @@ then
   /usr/bin/sysinfo
 else
   echo "starting bird"
-  ip addr add 172.18.0.13/32 dev lo || echo "probably the address is already assigned"
-  until fping -S 172.18.0.13 172.18.0.21 172.18.0.22 ; do echo "retry" ; done
+  # ip addr add 172.18.0.13/32 dev lo || echo "probably the address is already assigned"
+  # until fping -S 172.18.0.13 172.18.0.21 172.18.0.22 ; do echo "retry" ; sleep 10 ; done
+  # /bin/bash -il
   /usr/sbin/bird -d
 fi
