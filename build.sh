7ae5315f380919411a5aa67bcb9ba1954d2e1bfe
nic hart <nic@hdb3.net>
2020-04-17 14:53:08 +0100

    WiP

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-#!/bin/bash -xe
-REPO="localrepo:5000"
-baseimages="stack base"
-exportimages="kakapo relay hbgp hbgpdev bird2 bird frr bgpd"
-for i in $baseimages $exportimages ; do docker build -t $i $i ; done
-for ei in $exportimages ; do docker tag ${ei} ${REPO}/${ei}; docker push ${REPO}/${ei} ; done
+for image in base bird2 frr
+  do docker build -t $image $image
+done
